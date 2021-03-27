f959a5c1ec8379517b3a0dfa8fc5fb51a0e00b46
Cameron Nix <camerondeverenix@gmail.com>
2021-03-27 11:00:22 -0700

    updated search api call

--- server/apis/search_api.js
+++ server/apis/search_api.js
@@ -1,27 +1,24 @@
-// Api call to hubspot
+const API = require('../service/hubspot_constructor');
+const request = require('request');
 
-// Render search index
+// login
+exports.contactSearch = (req, res) => {
 
-exports.index = function(req, res) {
-	Search
-  .find()
-  .where('UserId').equals(req.user.id)
-  .then(function(dbTrip) {
-    res.render('trips/trips', {
-		  layout: 'main-trips',
-		  trip: dbTrip
-	  });
+  const apiParam = JSON.stringify(req.body.searchName); 
+  console.log(apiParam);
+
+  const showMe = API(apiParam);
+
+  request(showMe, function(error, response, body) {
+
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    const investor = [];
+    
+    for (var i = 0; i<body.results.length; i++) {
+      investor.push(body.results[i].properties) 
+    }
+
   });
-};
 
-exports.createTrip = function(req, res) {
-
-	// Add id from User onto req.body
-	req.body.UserId = req.user.id;
-
-  var newTrip = new Trip(req.body);
-
-  newTrip.save().then(function(dbPost) {
-    res.json(dbPost);
-  });
-};
+}
