6871ed1e227116b7fdcbeb47002c4b46ad74d8c8
Cameron Nix <camerondeverenix@gmail.com>
2021-04-01 21:12:41 -0700

    updated nav for authentication

--- app/components/children/Nav.js
+++ app/components/children/Nav.js
@@ -15,10 +15,7 @@ export default class Nav extends Component {
             <button type="button" className="navbar-toggle navbar-toggle-right" data-toggle="collapse" data-target=".navbar-main-collapse1">
               <i className="glyphicon glyphicon-plus"></i>
             </button>
-            {/*<Link to={"/"} ><img className="icon-middle-nav-mobile" src={require("../../img/shapes/shape.png")} /></Link>*/}
           </div>
-
-          {/* <div className="collapse navbar-collapse navbar-left navbar-main-collapse"> */}
           <div className="collapse navbar-collapse navbar-right navbar-main-collapse1">
             <ul className="nav navbar-nav">
               <li className="hidden">
