c1c36a07ed07f5c96ddb70b5d603f59fbdcc976d
Jaden Lee <alwaysjaden@gmail.com>
2021-04-01 22:59:52 -0700

    want to deploy to heroku

--- app/components/Mailinglist.js
+++ app/components/Mailinglist.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Redirect } from 'react-router-dom';
-import Auth from './utils/Auth';
 import Nav from './children/Nav'
-import Investors from './Mailinglist'
 import Modal from './modal/modal.js';
-
-import Card from '@material-ui/core/Card';
-import { makeStyles } from '@material-ui/core/styles';
-import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-
-import { Input, TextArea, FormBtn } from "./children/Form/";
 
 
 require('./Search.css');
