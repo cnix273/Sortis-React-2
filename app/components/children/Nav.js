0a76a412953debbb30973337318e35eb7f8a8647
Cameron Nix <camerondeverenix@gmail.com>
2021-10-26 23:22:38 -0700

    Block user from navigating to search or mailing list is not logged in and customize modal message to task performed

--- app/components/children/Nav.js
+++ app/components/children/Nav.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Modal from '../modal/modal';
 
 require('./nav.css');
 
 export default class Nav extends Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+			modalMessage: ""
+		};
+      this.showModalMail = this.showModalMail.bind(this);
+      this.showModalSearch = this.showModalSearch.bind(this);
+      this.hideModal = this.hideModal.bind(this);
+	}
+  
+  showModalMail = () => {
+    this.setState({
+      modalMessage: "Sorry, you must be logged in to add someone to the mailing list!",
+      show: true
+    });
+  };
+  showModalSearch = () => {
+    this.setState({
+      modalMessage: "Sorry, you must be logged in to perform a search!",
+      show: true
+    });
+  };
+
+  hideModal = () => {
+      this.setState({ show: false });
+  };
+
   render() {
     return (
       <nav className="navbar navbar-custom navbar-fixed-top" role="navigation">
@@ -19,12 +47,24 @@ export default class Nav extends Component {
               <li className="hidden">
                 <a href="#page-top"></a>
               </li>
-              <li>
-                <Link to={"/search"}><div className="page-scroll nav-left-text"><p>SEARCH</p></div></Link>
-              </li>
-              <li>
-                <Link to={"/mailinglist"}><div className="page-scroll nav-left-text"><p>MAILING-LIST</p></div></Link>
-              </li>
+              {this.props.authenticated ? (
+                <li>
+                  <Link to={"/search"}><div className="page-scroll nav-left-text"><p>SEARCH</p></div></Link>
+                </li>
+              ) : (
+                <li>
+                  <a href="#" onClick={this.showModalSearch}><div className="page-scroll nav-left-text"><p>SEARCH</p></div></a>
+                </li>
+              )}
+              {this.props.authenticated ? (
+                <li>
+                  <Link to={"/mailinglist"}><div className="page-scroll nav-left-text"><p>MAILING-LIST</p></div></Link>
+                </li>
+              ) : (
+                <li>
+                  <a href="#" onClick={this.showModalMail}><div className="page-scroll nav-left-text" data-toggle="modal"><p>MAILING-LIST</p></div></a>
+                </li>
+              )}
               {this.props.authenticated ? (
                 <li>
                   <a href="#" onClick={this.props.logout}><div className="page-scroll nav-left-text" data-toggle="modal"><p>LOGOUT</p></div></a>
@@ -37,6 +77,9 @@ export default class Nav extends Component {
             </ul>
           </div>
         </div>
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+            <p>{this.state.modalMessage}</p>
+        </Modal>
       </nav>
     );
   }
